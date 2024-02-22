61d734f844957ed8c31e6be691031d92990f185a
NicolasKouk <128096300+NicolasKouk@users.noreply.github.com>
2024-02-23 00:55:22 +0200

    tuples in list

--- bresenham.jl
+++ bresenham.jl
@@ -2,16 +2,15 @@ using Plots
 
 function f(x, y)
 	x = Int64(x); y = Int64(y);
-    if A[x,y] == 1
-		return 1
-	else
-		return 0
+	if (x,y) in A
+		return 1;
 	end
+	return 0;
 end
 
 # Bresenham algorithm for 1st octant
 function bresline1(x1, y1, x2, y2)
-	A = zeros(x2, y2);
+	A = []
 	dx = x2 - x1;
 	dy = y2 - y1;
 	x = x1;
@@ -20,23 +19,27 @@ function bresline1(x1, y1, x2, y2)
 	e = 2*dy - dx;
 	c2 = e - dx;
 	while x <= x2
-		A[x,y] = 1;
-		x = x +1;
+		A = [A;(x,y)];
+		x += 1;
 		if e < 0
-			e = e + c1;
+			e += c1;
 		else
-			y = y + 1;
-			e = e + c2;
+			y += 1;
+			e += c2;
 		end
 	end
 	return A;
 end
 
+# classic plot
+#x = range(1, 2*pi, length = 100)
+#plot(x, sin)
+
 # Enter here the coordinates of the 2 points.
-P1 = (1,1); 
+P1 = (0,0);
 P2 = (200,150);
 
-x = range(1, max(P1[1], P2[1]), length = max(P1[1], P2[1]))
-y = range(1, max(P1[2], P2[2]), length = max(P1[2], P2[2]))
+x = range(1, max(P1[1], P2[1]), length = max(P1[1], P2[1]));
+y = range(1, max(P1[2], P2[2]), length = max(P1[2], P2[2]));
 A = bresline1(P1[1], P1[2], P2[1], P2[2]);
 heatmap(x, y, f, c = :deep, aspect_ratio=:equal);
